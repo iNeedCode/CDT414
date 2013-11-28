f3a9efeac7ae573f344accc1d4b3419eaa20307f
Wonka <akhlaq87@gmail.com>
2013-11-29 00:32:01 +0100

    indentation

--- src/DateDiff.java
+++ src/DateDiff.java
@@ -4,26 +4,25 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-
 public class DateDiff {
-	
-	public Integer getDateDifference(String d1, String d2) throws ParseException  {
+
+	public Integer getDateDifference(String d1, String d2)
+			throws ParseException {
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-		
-		 Date date1 = df.parse(d1);
-	        Date date2 =  df.parse(d2);
-	        Calendar cal1 = Calendar.getInstance();
-	        Calendar cal2 = Calendar.getInstance();
-	        cal1.setTime(date1);
-	        cal2.setTime(date2);
-	 
-	        int numberOfDays = 0;
-	        while (cal1.before(cal2)) {
-	            numberOfDays++;
-	            cal1.add(Calendar.DATE,1);
-	        }
-	    
-		
+
+		Date date1 = df.parse(d1);
+		Date date2 = df.parse(d2);
+		Calendar cal1 = Calendar.getInstance();
+		Calendar cal2 = Calendar.getInstance();
+		cal1.setTime(date1);
+		cal2.setTime(date2);
+
+		int numberOfDays = 0;
+		while (cal1.before(cal2)) {
+			numberOfDays++;
+			cal1.add(Calendar.DATE, 1);
+		}
+
 		return numberOfDays;
 	}
 
